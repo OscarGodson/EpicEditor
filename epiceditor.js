f19b6a7b496df22b0232a7d595e4d0bbafd9832b
Adam bickford <xjunkyardx@gmail.com>
2012-02-29 18:19:07 -0800

    added keyboard shortcuts, not working fluidly in ie yet

--- epiceditor.js
+++ epiceditor.js
@@ -295,9 +295,26 @@
     iframeBody.style.margin = '0';
     _insertCSSLink(self.settings.basePath+self.settings.themes.editor,self.iframe);
     
-
     //Add a relative style to the overall wrapper to keep CSS relative to the editor
     self.iframe.getElementsByClassName('epiceditor-wrapper')[0].style.position = 'relative';
+
+    var isAlt = false;
+    self.iframe.body.addEventListener('keyup', function(e){
+      if(e.altKey) isAlt = false;
+    });
+    self.iframe.body.addEventListener('keydown', function(e){
+      if(e.altKey)
+        isAlt = true;
+      if(isAlt === true && e.keyCode === 80){
+        self.preview();
+        return false;
+      }
+      if(isAlt === true && e.keyCode === 69){
+        e.preventDefault() ? e.preventDefault() : e.returnValue = false;
+        self.edit();
+        return false;
+      }
+    })
 
     //Now grab the editor and previewer for later use
     this.editor = self.iframe.getElementsByClassName('epiceditor-textarea')[0];
