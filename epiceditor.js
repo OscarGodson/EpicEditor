50d388437922eef7a0e2c535fa47559f5a639c40
Adam bickford <xjunkyardx@gmail.com>
2012-03-16 20:41:35 -0700

    added a catch for ctrl/cmd + s so it doesn't do anything when accidentally pressed

--- epiceditor.js
+++ epiceditor.js
@@ -551,27 +551,29 @@
 
     //Add keyboard shortcuts for convenience.
     var isMod = false;
-
+    var isCtrl = false;
     self.iframe.addEventListener('keyup', function(e){
-      if(e.keyCode === self.settings.shortcut.modifier){ isMod = false };
+      if(e.keyCode == self.settings.shortcut.modifier){ isMod = false };
+      if(e.keyCode == 17){ isCtrl = false };
     });
     self.iframe.addEventListener('keydown', function(e){
-      if(e.keyCode === self.settings.shortcut.modifier){ isMod = true }; //check for modifier press(default is alt key), save to var
+      if(e.keyCode == self.settings.shortcut.modifier){ isMod = true }; //check for modifier press(default is alt key), save to var
+      if(e.keyCode == 17){ isCtrl = true }; //check for ctrl/cmnd press, in order to catch ctrl/cmnd + s
 
       //Check for alt+p and make sure were not in fullscreen - default shortcut to switch to preview
-      if(isMod === true && e.keyCode === self.settings.shortcut.preview && !self.eeState.fullscreen){
+      if(isMod == true && e.keyCode == self.settings.shortcut.preview && !self.eeState.fullscreen){
         e.preventDefault();
         self.preview();
       }
       //Check for alt+o - default shortcut to switch back to the editor
-      if(isMod === true && e.keyCode === self.settings.shortcut.edit){
+      if(isMod == true && e.keyCode == self.settings.shortcut.edit){
         e.preventDefault();
         if(!self.eeState.fullscreen){
           self.edit();
         }
       }
       //Check for alt+f - default shortcut to make editor fullscreen
-      if(isMod === true && e.keyCode === self.settings.shortcut.fullscreen){
+      if(isMod == true && e.keyCode == self.settings.shortcut.fullscreen){
         e.preventDefault();
         _goFullscreen(fsElement);
       }
@@ -581,6 +583,11 @@
         if(!document.body.webkitRequestFullScreen){
           _exitFullscreen(fsElement);
         }
+      }
+
+      //Check for ctrl/cmnd + s (since a lot of people do it out of habit) and make it do nothing
+      if(isCtrl == true && e.keyCode == 83){
+        e.preventDefault();
       }
     });
 
