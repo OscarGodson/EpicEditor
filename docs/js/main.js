f75089859088c6e0af8c514413c678940ad91c4e
John Donahue <john.m.donahue@gmail.com>
2012-05-13 23:49:59 -0700

    add zip, download links, and rebuild

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,9 +1,14 @@
 $(function () {
+  var zipUrl = 'docs/downloads/EpicEditor-v' + EpicEditor.version + '.zip';
+  
   $('#wrapper').before('<div id="toc"><h2><a href="#">EpicEditor</a></h2><ul id="toc-list"></ul></div>');
-  $($('h1')[0]).append('<span>beta '+EpicEditor.version+'</span>');
+  $($('h1')[0]).append('<span>beta ' + EpicEditor.version + '</span>');
+  $($('h1')[0]).after('<div id="download"><a href="' + zipUrl + '">Download the .zip</a></div>');
+  $($('h3')[0]).next().html('<a href="' + zipUrl + '">Download the .zip</a> or clone the repo:');
   $($('h2')[3]).before('<button id="try-it">Try it!</button><div id="epiceditor"></div>');
   $($('h2')[2]).before('<div id="example-1"></div>');
   $('tr:even').addClass('even');
+  
   var opts = {
       container: 'example-1'
     , file:{
@@ -42,8 +47,8 @@ $(function () {
 
     if (idx > 1) {
       h.attr("id", link);
+      h.html('<a href="#' + link + '">' + title + '</a>');
       $("#toc-list").append('<li class="toc-' + this.nodeName.toLowerCase() + '"><a id="" href="#' + link + '">' + title + '</a></li>');
-      $(this).html('<a href="#' + link + '">' + title + '</a>');
     }
   });
   
