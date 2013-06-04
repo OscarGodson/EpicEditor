2956e403c4b1d13d898da18967bfb5fc6c4d2006
Oscar Godson <oscargodson@gmail.com>
2013-06-04 02:05:59 -0700

    Ticket #165 - Added passing tests.

--- test/test.load.js
+++ test/test.load.js
@@ -27,7 +27,7 @@ describe(".load([callback])", function () {
     editorIframe = getContainer(id).getElementsByTagName('iframe');
     editorInnards = getIframeDoc(editorIframe[0]);
     done();
-  })
+  });
 
   after(function (done) {
     editor.unload();
@@ -78,5 +78,78 @@ describe(".load([callback])", function () {
     editor.load();
     expect(editor.getElement('editorIframe').style.left).to.be('-999999px');
     expect(editor.getElement('previewerIframe').style.left).to.be('');
+  });
+
+  describe('anchors', function () {
+    var previewer, anchor, oldScrollPos, originalHostname;
+    before(function () {
+      previewer = editor.getElement('previewer');
+      editor.importFile(id, '<a href="#test">foo</a>\n\nfoo\n\nbar\n\n<h1 id="test">Test</h1>\n\nhello\n\nworld\n\nblah\n\nblah');
+      editor.element.style.height = '10px';
+      editor.reflow().preview();
+      anchor = previewer.body.querySelector('a');
+      // Fucking IE bug.
+      // If you change the href of an <a> it'll set the hostname to be blank.
+      // Stash the original one here and reset in the beforeEach()
+      originalHostname = anchor.hostname
+    });
+    beforeEach(function () {
+      anchor.href = "#test";
+      // Make sure this comes after changing the href
+      anchor.hostname = originalHostname
+      anchor.target = '';
+      previewer.body.scrollTop = 0;
+    });
+    it('scrolls the previewer body if the anchor is a local hash link', function () {
+      oldScrollPos = previewer.body.scrollTop;
+      expect(oldScrollPos).to.be(0);
+      anchor.click();
+      expect(previewer.body.scrollTop).to.be.greaterThan(0);
+      previewer.body.scrollTop = 0;
+
+      anchor.href = window.location.hostname+'#test';
+      // Again, this is for IE
+      anchor.hostname = originalHostname
+      anchor.click();
+      expect(previewer.body.scrollTop).to.be.greaterThan(0);
+
+    });
+    it('does NOT scroll the previewer if the anchor is on an enternal link', function () {
+      oldScrollPos = previewer.body.scrollTop;
+      expect(oldScrollPos).to.be(0);
+
+      anchor.href = 'http://google.com';
+      // Again, this is for IE
+      anchor.hostname = originalHostname
+      anchor.click();
+      expect(previewer.body.scrollTop).to.be(0);
+      previewer.body.scrollTop = 0;
+
+      anchor.href = 'http://google.com#foo';
+      // Again, this is for IE
+      anchor.hostname = originalHostname
+      anchor.click();
+      expect(previewer.body.scrollTop).to.be(0);
+    });
+    it('does NOT scroll if there is no matching ID', function () {
+      oldScrollPos = previewer.body.scrollTop;
+      expect(oldScrollPos).to.be(0);
+      anchor.href = '#poop';
+      // Again, this is for IE
+      anchor.hostname = originalHostname
+      anchor.click();
+      expect(previewer.body.scrollTop).to.be(0);
+    });
+    it('sets the target of the link to _self if its a local hash link', function () {
+      anchor.click();
+      expect(anchor.target).to.be('_self');
+    });
+    it('does NOT set the target of the link to _self if its a local hash link', function () {
+      anchor.href = 'http://google.com';
+      // Again, this is for IE
+      anchor.hostname = originalHostname
+      anchor.click();
+      expect(anchor.target).to.be('');
+    });
   });
 });
