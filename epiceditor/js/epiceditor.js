a29595ba619daa7e3ea652e7864d4755e546547b
Patrick Bougie <patrick@bougie.ca>
2013-06-29 09:21:45 -0400

    Change span to button tag.

--- epiceditor/js/epiceditor.js
+++ epiceditor/js/epiceditor.js
@@ -534,9 +534,9 @@
                   '<iframe frameborder="0" id="epiceditor-editor-frame"></iframe>' +
                   '<iframe frameborder="0" id="epiceditor-previewer-frame"></iframe>' +
                   '<div id="epiceditor-utilbar">' +
-                    (self._previewEnabled ? '<span title="' + this.settings.string.togglePreview + '" class="epiceditor-toggle-btn epiceditor-toggle-preview-btn"></span> ' : '') +
-                    (self._editEnabled ? '<span title="' + this.settings.string.toggleEdit + '" class="epiceditor-toggle-btn epiceditor-toggle-edit-btn"></span> ' : '') +
-                    (self._fullscreenEnabled ? '<span title="' + this.settings.string.toggleFullscreen + '" class="epiceditor-fullscreen-btn"></span>' : '') +
+                    (self._previewEnabled ? '<button title="' + this.settings.string.togglePreview + '" class="epiceditor-toggle-btn epiceditor-toggle-preview-btn"></button> ' : '') +
+                    (self._editEnabled ? '<button title="' + this.settings.string.toggleEdit + '" class="epiceditor-toggle-btn epiceditor-toggle-edit-btn"></button> ' : '') +
+                    (self._fullscreenEnabled ? '<button title="' + this.settings.string.toggleFullscreen + '" class="epiceditor-fullscreen-btn"></button>' : '') +
                   '</div>' +
                 '</div>'
     
