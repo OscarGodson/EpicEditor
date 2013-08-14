74760f4e6de7ea9c9b3749b9511cd1f8ba94109c
Oscar Godson <oscargodson@gmail.com>
2013-08-14 02:51:23 -0700

    Package and release EpicEditor 0.2.2

--- epiceditor/js/epiceditor.js
+++ epiceditor/js/epiceditor.js
@@ -1838,7 +1838,7 @@
     this.getElement('previewer').documentElement.style.overflow = setting;
   }
 
-  EpicEditor.version = '0.2.1';
+  EpicEditor.version = '0.2.2';
 
   // Used to store information to be shared across editors
   EpicEditor._data = {};
